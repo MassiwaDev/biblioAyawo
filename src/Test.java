73f23aea5bb39173a4717c0277ae1e0334b4b9c1
ayawo_2015 <wisdman2004@yahoo.fr>
2015-04-07 15:25:24 +0200

    nouveau commentaire 15h25

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,8 @@
-
+ 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Commentaire modifié le 07/04/2015 à 15h24
 		System.out.println("hello Patrice is not speaking anymore");
 		System.out.println("After Modif");
 		System.out.println("Le fecth ne fonctionne pas.");
