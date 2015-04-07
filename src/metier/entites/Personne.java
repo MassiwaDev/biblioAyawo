8d25433fcf91868eb12f126c9723fe881fbb7dc2
ayawo_2015 <wisdman2004@yahoo.fr>
2015-04-07 16:37:16 +0200

    creation de Personne

---
+++ src/metier/entites/Personne.java
@@ -0,0 +1,96 @@
+package metier.entites;
+
+import java.util.Date;
+
+public class Personne {
+	
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	private String sexe;
+	
+	
+	
+	
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getSexe() {
+		return sexe;
+	}
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((dateNaissance == null) ? 0 : dateNaissance.hashCode());
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+		result = prime * result + ((sexe == null) ? 0 : sexe.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personne other = (Personne) obj;
+		if (dateNaissance == null) {
+			if (other.dateNaissance != null)
+				return false;
+		} else if (!dateNaissance.equals(other.dateNaissance))
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (prenom == null) {
+			if (other.prenom != null)
+				return false;
+		} else if (!prenom.equals(other.prenom))
+			return false;
+		if (sexe == null) {
+			if (other.sexe != null)
+				return false;
+		} else if (!sexe.equals(other.sexe))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Personne [nom=" + nom + ", prenom=" + prenom
+				+ ", dateNaissance=" + dateNaissance + ", sexe=" + sexe + "]";
+	}
+	
+	
+	
+	
+
+}
