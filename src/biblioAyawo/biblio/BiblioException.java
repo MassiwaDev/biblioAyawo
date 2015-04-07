4644b11684a649a7d7bfe9781b2b763a7071695e
ayawo_2015 <wisdman2004@yahoo.fr>
2015-04-07 16:08:41 +0200

    creation de biblioException

---
+++ src/biblioAyawo/biblio/BiblioException.java
@@ -0,0 +1,18 @@
+package biblioAyawo.biblio;
+
+public class BiblioException extends Exception {
+
+	
+	public BiblioException() {
+		super();
+
+	}
+
+	public BiblioException(String message) {
+		super(message);
+
+	}
+	
+	
+
+}
