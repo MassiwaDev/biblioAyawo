5c65de7fceaa7677779f5d22a5b0503a725aafc8
MassiwaDev <assad.yssouf@gmail.com>
2015-04-07 15:05:14 +0200

    Modofic fichier test

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello Patrice is not speaking anymore");
+		System.out.println("After Modif");
+		System.out.println("Le fecth ne fonctionne pas.");
 	}
 
 }
