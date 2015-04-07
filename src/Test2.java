a0b5b133b701f49bfcfde2f0a105d64e2077da30
stagiaire <pjournou.afpa@gmail.com>
2015-04-07 14:43:49 +0200

    fin de communication

--- src/Test2.java
+++ src/Test2.java
@@ -2,9 +2,8 @@
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("hello Patrice is speaking");
+		System.out.println("hello Patrice is not speaking anymore");
 		System.out.println("After Modif");
-
 	}
 
 }
