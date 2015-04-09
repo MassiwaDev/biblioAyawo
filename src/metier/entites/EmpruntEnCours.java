6bad926b9632840ec7b58ca0a80064fdef3e8441
MassiwaDev <assad.yssouf@gmail.com>
2015-04-09 10:41:47 +0200

    Ajout classe EmpruntEnCours

--- src/metier/entites/EmpruntEnCours.java
+++ src/metier/entites/EmpruntEnCours.java
@@ -1,14 +1,39 @@
 package metier.entites;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class EmpruntEnCours {
 
 	private Date dateEmprunt;
 
+	private Utilisateur emprunteur;
+	
 	public EmpruntEnCours(Date dateEmprunt) {
 		super();
 		this.dateEmprunt = dateEmprunt;
+	}
+
+	public Date getDateEmprunt() {
+		return dateEmprunt;
+	}
+
+	public void setDateEmprunt(Date dateEmprunt) {
+		this.dateEmprunt = dateEmprunt;
+	}
+	
+	
+	public Utilisateur getEmprunteur() {
+		return emprunteur;
+	}
+
+	public void setEmprunteur(Utilisateur emprunteur) {
+		this.emprunteur = emprunteur;
+	}
+
+	@Override
+	public String toString() {
+		return "L'exemplaire est actuellement emprunté par : "+getEmprunteur()+" depuis le : " + getDateEmprunt() + ".";
 	}
 	
 	
