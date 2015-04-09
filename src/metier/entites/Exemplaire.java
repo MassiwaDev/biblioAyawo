25845b639f4e7aac3c9d1876ce6f368e59352b49
MassiwaDev <assad.yssouf@gmail.com>
2015-04-09 10:30:56 +0200

    Classe exemplaire v1.0

--- src/metier/entites/Exemplaire.java
+++ src/metier/entites/Exemplaire.java
@@ -1,5 +1,86 @@
 package metier.entites;
 
-public class Exemplaire {
+import java.util.Date;
 
+public class Exemplaire {
+	private int idExemplaire;
+	private Date dateAchat;
+	private EnumStatusExemplaire status;
+	private String isbn;
+	
+	
+	private EmpruntEnCours empruntEnCours;
+
+
+	public Exemplaire(int idExemplaire, Date dateAchat,
+			EnumStatusExemplaire status, String isbn) {
+		super();
+		this.idExemplaire = idExemplaire;
+		this.dateAchat = dateAchat;
+		this.status = status;
+		this.isbn = isbn;
+	}
+
+
+	public int getIdExemplaire() {
+		return idExemplaire;
+	}
+
+
+	public Date getDateAchat() {
+		return dateAchat;
+	}
+
+
+	public EnumStatusExemplaire getStatus() {
+		return status;
+	}
+
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+
+	public void setIdExemplaire(int idExemplaire) {
+		this.idExemplaire = idExemplaire;
+	}
+
+
+	public void setDateAchat(Date dateAchat) {
+		this.dateAchat = dateAchat;
+	}
+
+
+	public void setStatus(EnumStatusExemplaire status) {
+		this.status = status;
+	}
+
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+
+	public EmpruntEnCours getEmpruntEnCours() {
+		return empruntEnCours;
+	}
+
+
+	public void setEmpruntEnCours(EmpruntEnCours empruntEnCours) {
+		this.empruntEnCours = empruntEnCours;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Il s'agit de l'exemplaire, idExemplaire :" + getIdExemplaire() + ", sa date d'achat est :"
+				+ getDateAchat() + " son statut est : " + getStatus() + ", son isbn est :" + getIsbn()
+				+ ". Il est actuellement : " + getEmpruntEnCours() + ".";
+	}
+	
+	
+	
+	
+	
 }
