4e5190e0dad9904d411ce0f9a52b24f7ed8337a9
ayawo_2015 <linogagli01@gmail.com>
2015-04-09 09:51:53 +0200

    test

---
+++ src/metier/dao/UtilisateurDao.java
@@ -0,0 +1,18 @@
+package metier.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import metier.entites.Utilisateur;
+
+public class UtilisateurDao {
+	
+	private List <Utilisateur> listUtilisateurs = new ArrayList<Utilisateur>();
+	
+	private void initListe(){
+		/*Utilisateur u1 = new Utilisateur()
+		listUtilisateurs.add()*/
+		
+	}
+
+}
