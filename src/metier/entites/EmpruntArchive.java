3115131d04f14c0e6e1499cc222a2eee59f5dcf4
MassiwaDev <assad.yssouf@gmail.com>
2015-04-09 11:29:57 +0200

    Ajout des attributs de la classe EmpruntArchive.

--- src/metier/entites/EmpruntArchive.java
+++ src/metier/entites/EmpruntArchive.java
@@ -1,5 +1,82 @@
 package metier.entites;
 
-public class EmpruntArchive {
+import java.util.Date;
 
+public class EmpruntArchive {
+	private Date dateRestitutionEff;
+	private Date dateEmprunt;
+	
+	private Exemplaire exemplaire;
+	private Utilisateur emprunteur;
+	
+	
+	
+	public EmpruntArchive(Date dateRestitutionEff, Date dateEmprunt) {
+		super();
+		this.dateRestitutionEff = dateRestitutionEff;
+		this.dateEmprunt = dateEmprunt;
+	}
+
+
+
+	public Date getDateRestitutionEff() {
+		return dateRestitutionEff;
+	}
+
+
+
+	public Date getDateEmprunt() {
+		return dateEmprunt;
+	}
+
+
+
+	public Exemplaire getExemplaire() {
+		return exemplaire;
+	}
+
+
+
+	public Utilisateur getEmprunteur() {
+		return emprunteur;
+	}
+
+
+
+	public void setDateRestitutionEff(Date dateRestitutionEff) {
+		this.dateRestitutionEff = dateRestitutionEff;
+	}
+
+
+
+	public void setDateEmprunt(Date dateEmprunt) {
+		this.dateEmprunt = dateEmprunt;
+	}
+
+
+
+	public void setExemplaire(Exemplaire exemplaire) {
+		this.exemplaire = exemplaire;
+	}
+
+
+
+	public void setEmprunteur(Utilisateur emprunteur) {
+		this.emprunteur = emprunteur;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Emprunts Archivés : "
+				+ "L'Utilisateur " + getEmprunteur() +" a emprunté l'exemplaire : "+ getExemplaire()+", le : " +getDateEmprunt() +". Il l'a restitué le : "
+				+ getDateRestitutionEff() + ",";
+	}
+	
+	
+	
+	
+	
+	
 }
