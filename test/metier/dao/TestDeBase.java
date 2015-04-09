11b0e2f18160fdf1142fb5f06617b1686148ac81
ayawo_2015 <linogagli01@gmail.com>
2015-04-09 14:53:59 +0200

    Première livraison de TestDeBase

--- test/metier/dao/TestDeBase.java
+++ test/metier/dao/TestDeBase.java
@@ -1,9 +1,65 @@
 package metier.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import metier.entites.Adherent;
+import metier.entites.BiblioException;
+import metier.entites.Employe;
+import metier.entites.EmpruntEnCours;
+import metier.entites.Exemplaire;
+import metier.entites.Utilisateur;
+
 public class TestDeBase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		UtilisateursDao utilisateursDao = new UtilisateursDao();
+		ExemplairesDao exemplairesDao = new ExemplairesDao();
+		
+		//Demande de deux exemplaires par leur id aux Dao
+		Exemplaire ex1 = null;
+		try {
+			ex1 = exemplairesDao.findByKey(1);
+			//System.out.println(ex1);
+		} catch (BiblioException e2) {
+			System.err.println(e2.getMessage());
+		}
+		
+		Exemplaire ex2 = null;
+		try {
+			ex2 = exemplairesDao.findByKey(2);
+		//	System.out.println(ex2);
+		} catch (BiblioException e2) {
+			System.err.println(e2.getMessage());
+		}
+		
+		//Demande d'un adhérent par son id aux Dao
+		Adherent ad1 = null;
+		try {
+			ad1 = (Adherent) utilisateursDao.findByKey(3);
+			//System.out.println(ad1);
+		} catch (BiblioException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		//Demande d'un employé par son id aux Dao
+		Employe emp1 = null;		
+		try {
+			emp1 = (Employe) utilisateursDao.findByKey(1);
+			//System.out.println(emp1);
+		} catch (BiblioException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		//Création d'un emprunt en cours pour un adhérent
+		EmpruntEnCours emprunt1 = new EmpruntEnCours(ad1, ex1, new Date());
+		
+		//Création d'un emprunt en cours pour un employé
+		EmpruntEnCours emprunt2 = new EmpruntEnCours(emp1, ex2, new Date());
+		
+		System.out.println(emprunt1);
+		System.out.println(emprunt2);
 
 	}
 
